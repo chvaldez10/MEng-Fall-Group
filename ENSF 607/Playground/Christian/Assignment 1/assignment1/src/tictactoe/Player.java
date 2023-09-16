8710b570d11860e6f4ba95fc24103cf77bfe008c
chvaldez10 <christianvaldez021@gmail.com>
2023-09-16 12:06:22 -0600

    revised player class to handle overlaps

--- ENSF 607/Playground/Christian/Assignment 1/assignment1/src/tictactoe/Player.java
+++ ENSF 607/Playground/Christian/Assignment 1/assignment1/src/tictactoe/Player.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * Represents a player in the Tic Tac Toe game.
  * Each player has a name and a mark ('X' or 'O').
  */
-public class Player {
+public class Player implements Constants{
     /** The name of the player. */
     private String name;
 
@@ -20,7 +20,7 @@ public class Player {
     private char mark;
 
     /** Scanner for getting input from the player. */
-    Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
 
 
     /**
@@ -54,19 +54,16 @@ public class Player {
 
 
     /**
-     * Starts the turn for this player. If the board isn't full and there's no winner yet,
-     * the player is prompted to make a move. After the move, it checks for a winner or draw.
+     * Initiates the player's turn. If the game can continue, it prompts the player to make a move.
+     * After the move, it checks for a game conclusion and, if necessary, displays the result.
      */
     public void play() {
-        // board has space and no winner
-        if (!board.isFull() && !board.oWins() && !board.xWins()) makeMove();
+        if (isGameContinuable()) {
+            makeMove();
+        }
 
-        if (board.xWins()) {
-            System.out.println("X Wins!");
-        } else if (board.oWins()){
-            System.out.println("O Wins!");
-        } else if (board.isFull()){
-            System.out.println("Draw!");
+        if (isGameOver()) {
+            displayGameResult();
         } else {
             board.display();
             opponent.play();
@@ -74,12 +71,62 @@ public class Player {
     }
 
     /**
+     * Checks if the game can continue. The game can continue if the board isn't full and there's no winner yet.
+     *
+     * @return true if the game can continue, false otherwise.
+     */
+    private boolean isGameContinuable() {
+        return !board.isFull() && !board.oWins() && !board.xWins();
+    }
+
+    /**
+     * Determines if the game has concluded, either due to a win or a draw.
+     *
+     * @return true if the game is over, false otherwise.
+     */
+    private boolean isGameOver() {
+        return board.isFull() || board.oWins() || board.xWins();
+    }
+
+    /**
+     * Displays the result of the game. This method determines which player (if any) won the game,
+     * or if the game ended in a draw, and then prints an appropriate message.
+     * <p>
+     * If player X wins, it displays "THE GAME IS OVER: [X's name] is the winner!"
+     * If player O wins, it displays "THE GAME IS OVER: [O's name] is the winner!"
+     * If the board is full and neither player has won, it displays "Draw!"
+     * </p>
+     */
+    private void displayGameResult() {
+        if (board.xWins()) {
+            System.out.println("THE GAME IS OVER: " + (mark == 'X' ? name : opponent.name) + " is the winner!");
+        } else if (board.oWins()) {
+            System.out.println("THE GAME IS OVER: " + (mark == 'O' ? name : opponent.name) + " is the winner!");
+        } else {
+            System.out.println("Draw!");
+        }
+    }
+
+    /**
      * Allows the player to make a move on the board.
      */
     public void makeMove() {
-        int tmpMarkRow = getUserInput("row");
-        int tmpMarkColumn = getUserInput("column");
-        board.addMark(tmpMarkRow, tmpMarkColumn, mark);
+        int tmpMarkRow;
+        int tmpMarkColumn;
+        char tmpMark;
+
+        while (true) {
+            tmpMarkRow = getUserInput("row");
+            tmpMarkColumn = getUserInput("column");
+            tmpMark = board.getMark(tmpMarkRow, tmpMarkColumn);
+
+            if (tmpMark == SPACE_CHAR) {
+                board.addMark(tmpMarkRow, tmpMarkColumn, mark);
+                break; // exits the loop when a valid spot is marked
+            } else {
+                System.out.println("Spot not available. Try again.");
+            }
+        }
     }
 
     /**
