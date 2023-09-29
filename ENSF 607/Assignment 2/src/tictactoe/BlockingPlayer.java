35dbeabaf83694d854dd1d618c6309443f037970
rrsantillan <132472698+rrsantillan@users.noreply.github.com>
2023-09-29 00:28:53 -0600

    started BlockingPlayer implementation

--- ENSF 607/Assignment 2/src/tictactoe/BlockingPlayer.java
+++ ENSF 607/Assignment 2/src/tictactoe/BlockingPlayer.java
@@ -1,10 +1,30 @@
 package tictactoe;
 
-public class BlockingPlayer extends Player{
+public class BlockingPlayer extends RandomPlayer{
 	public BlockingPlayer(String name, char mark) {
 		super(name, mark);
 	}
 	
 	@Override
-	public void makeMove() {}
+	public void makeMove() {
+		
+		for (int row = lowIndex; row <= highIndex; row ++) {
+			for (int col = lowIndex; col <= highIndex; col++) {
+				if (testforBlocking(row, col) == true) {
+					board.addMark(row, col, mark);
+				} else {
+					super.makeMove();
+				}
+			}
+		}
+		// Try blocking first. If no blocking, then revert to using super.makeMove().
+	}
+	
+	/**
+	 * @return True if there is an opportunity to block.
+	 */
+	protected boolean testforBlocking(int row, int col) {
+			board.getMark(row, col);
+		return false;
+	}
 }
